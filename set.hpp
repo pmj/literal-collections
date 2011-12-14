9689b863e3e82b35c9780709e7a882364d01fdae
Phil Jordan <phil@philjordan.eu>
2011-12-14 22:42:52 +0100

    Literal syntax for creating pre-populated std::set objects.

---
+++ set.hpp
@@ -0,0 +1,44 @@
+//
+//  set.hpp
+//  ssdcache
+//
+//  Created by Phillip Jordan on 12/14/11.
+//  Copyright (c) 2011 Phillip Jordan. All rights reserved.
+//
+
+#ifndef LITERAL_COLLECTIONS_SET_HPP
+#define LITERAL_COLLECTIONS_SET_HPP
+
+/* convenient syntax for creating pre-populated (literal) STL std::set collections
+ * 
+ * // create a set of integers containing the numbers 1-4
+ * std::set<int> my_set = literal::set<int>(1, 2, 3, 4);
+ *
+ * For even shorter syntax, try
+ * namespace l = literal;
+ */
+
+namespace literal
+{
+	template <typename T>
+		inline void populate_set(std::set<T>& coll)
+	{}
+	
+	template <typename T, typename Arg_T, typename... RestTypes>
+		inline void populate_set(std::set<T>& coll, Arg_T arg, RestTypes... rest_args)
+	{
+		coll.insert(arg);
+		populate_set(coll, rest_args...);
+	}
+	
+	template <typename T, typename... ElementTypes>
+		std::set<T> set(ElementTypes... els)
+	{
+		std::set<T> coll;
+		populate_set(coll, els...);
+		return coll;
+	}
+}
+
+
+#endif
